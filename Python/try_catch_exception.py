607edde2fae45693355a9d520b488d04f137d6c8
enesoriginal <enescavusoriginal@gmail.com>
2022-06-14 10:00:09 +0300

    try catch

---
+++ Python/try_catch_exception.py
@@ -0,0 +1,12 @@
+# hackerrank Inheritance problem solution
+# problem link --- > https://www.hackerrank.com/challenges/30-exceptions-string-to-integer/problem?isFullScreen=true
+
+S = input()
+try:
+  print(int(S))
+except ValueError:
+  print("Bad String")
+
+#check out the source code - and problem page on hackerrank link below
+
+#### https://www.hackerrank.com/challenges/30-exceptions-string-to-integer/problem?isFullScreen=true 
